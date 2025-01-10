e4415db0f5c0d61ff6f8642e6568e70e8835bf6b
Pathi Swapna <pathiswoopna@gmail.com>
2025-01-10 11:16:14 +0530

    first commit

---
+++ backend/emailService.js
@@ -0,0 +1,19 @@
+const nodemailer=require("nodemailer")
+
+const transporter=nodemailer.createTransport({
+    service:"Gmail",
+    auth:{
+        user:"pathiswoopna@gmail.com",
+        pass:"puzplslltsmblops"
+    }
+})
+
+
+
+
+
+
+
+
+
+
